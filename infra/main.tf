76ef5fb54167a3a5778b2e15909cf60d90d810f2
Gleb Kikot <glebkikot200@gmail.com>
2023-01-10 21:50:16 +0200

    change

---
+++ infra/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = var.GITHUB_TOKEN
+}
+
+terraform {
+  backend "s3" {
+    bucket = "petproject-terraform" 
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+resource "github_repository_environment" "example" {
+  environment  = terraform.workspace
+  repository   = var.repository
+}
+
+resource "github_actions_environment_secret" "github_token" {
+  repository        = var.repository
+  environment       = github_repository_environment.example.environment
+  secret_name       = "GH_TOKEN"
+  plaintext_value   = var.GITHUB_TOKEN
+}
+
+resource "github_actions_environment_secret" "aws_access_key_id" {
+  repository        = var.repository
+  environment       = github_repository_environment.example.environment
+  secret_name       = "AWS_ACCESS_KEY_ID"
+  plaintext_value   = var.AWS_ACCESS_KEY_ID
+}
+
+resource "github_actions_environment_secret" "aws_secret_access_key" {
+  repository        = var.repository
+  environment       = github_repository_environment.example.environment
+  secret_name       = "AWS_SECRET_ACCESS_KEY"
+  plaintext_value   = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_ecr_repository" "server" {
+  name                 = "server"
+}
+
+resource "github_actions_environment_secret" "repository_url" {
+  repository        = var.repository
+  environment       = github_repository_environment.example.environment
+  secret_name       = "REPOSITORY_URL"
+  plaintext_value   = aws_ecr_repository.server.repository_url
+}
+
+
+
---
+++ infra/variables.tf
@@ -0,0 +1,13 @@
+variable "GITHUB_TOKEN" {
+}
+
+variable "repository" {
+  default = "petproject"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    
+}
